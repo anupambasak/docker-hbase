ffa31c927375782399763f74f20e7f3ff51c1d06
Anupam Basak <basak.anupam@cris.org.in>
2016-12-21 17:52:28 +0530

    proxy added

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd jdk8
-docker build -t alpine-jdk8 --force-rm .
+dname=$RANDOM
+cat Dockerfile | envsubst | tee $dname
+docker build -f $dname -t alpine-jdk8 --force-rm .
+rm -v $dname
